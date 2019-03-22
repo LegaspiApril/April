23806670cda56179f9eda2aaa79a27e7b7d70971
bryan <brybryan>
2019-03-22 10:45:05 +0800

    MygitHub

---
+++ application/views/myHub.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/js/javascript.js">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<title>My Github</title>
+</head>
+<body>
+	<div class="yes">
+	<h1>Hello</h1>
+	<p>My first github</p>
+	</div>
+</body>
+</html>
